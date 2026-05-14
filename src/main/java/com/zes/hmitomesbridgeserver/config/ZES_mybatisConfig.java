d93a12b85ad2f28196731bfb9523b7ab94293c37
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-05-14 13:35:37 +0900

    Add OPC-UA server module with DB-backed WorkItem provider skeleton

---
+++ src/main/java/com/zes/hmitomesbridgeserver/config/ZES_mybatisConfig.java
@@ -0,0 +1,38 @@
+package com.zes.hmitomesbridgeserver.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.zes.hmitomesbridgeserver.mapper"})
+public class ZES_mybatisConfig
+{
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception
+    {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        return bean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory factory)
+    {
+        return new SqlSessionTemplate(factory);
+    }
+
+    @Bean
+    public DataSourceTransactionManager txManager(DataSource dataSource)
+    {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
