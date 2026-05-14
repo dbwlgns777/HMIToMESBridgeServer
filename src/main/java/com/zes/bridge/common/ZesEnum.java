4ee965a56fab086d908ffb98f04a9e3da8353278
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-05-14 12:25:30 +0900

    Delete src/main/java/com/zes/bridge directory

--- src/main/java/com/zes/bridge/common/ZesEnum.java
+++
@@ -1,8 +0,0 @@
-package com.zes.bridge.common;
-
-public enum ZesEnum {
-    ZES_SUCCESS,
-    ZES_VALID_ERROR,
-    ZES_NOT_FOUND,
-    ZES_SERVER_ERROR
-}
--- src/main/java/com/zes/bridge/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.zes.bridge.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.zes.bridge.mapper")
-public class MyBatisConfig {
-}
