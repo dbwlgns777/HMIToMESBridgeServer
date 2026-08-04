18f1c2eca2602f64592df6b20551e3dc25b82a42
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-08-04 13:27:10 +0900

    Remove unused JPA startup dependency

--- src/main/java/com/zes/hmitomesbridgeserver/config/ZES_authenticationDataSource.java
+++ src/main/java/com/zes/hmitomesbridgeserver/config/ZES_authenticationDataSource.java
@@ -2,35 +2,29 @@ package com.zes.hmitomesbridgeserver.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(
-        entityManagerFactoryRef = "entityManagerFactory",
-        basePackages = {"com.zes.hmitomesbridgeserver.structure"},
-        transactionManagerRef = "transactionManager"
-)
 public class ZES_authenticationDataSource
 {
+    private static final String MYSQL_DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+
     @Primary
     @Bean(name = "dataSource")
     public DataSource dataSource()
     {
         try
         {
+            // Load and register Connector/J before Hikari asks DriverManager for the JDBC URL driver.
+            Class.forName(MYSQL_DRIVER_CLASS_NAME);
             HikariConfig config = new HikariConfig("db_config/mysql.config");
+            config.setDriverClassName(MYSQL_DRIVER_CLASS_NAME);
             return new HikariDataSource(config);
         }
         catch (Exception e)
@@ -39,24 +33,4 @@ public class ZES_authenticationDataSource
         }
     }
 
-    @Primary
-    @Bean(name = "entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-            EntityManagerFactoryBuilder builder,
-            @Qualifier("dataSource") DataSource dataSource)
-    {
-        return builder
-                .dataSource(dataSource)
-                .packages("com.zes.hmitomesbridgeserver.structure")
-                .persistenceUnit("authentication")
-                .build();
-    }
-
-    @Primary
-    @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager(
-            @Qualifier("entityManagerFactory") jakarta.persistence.EntityManagerFactory entityManagerFactory)
-    {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
 }
