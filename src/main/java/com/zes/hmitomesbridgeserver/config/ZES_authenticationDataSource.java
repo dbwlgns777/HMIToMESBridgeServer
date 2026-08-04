812119c014e6eb8450a0716d2b88e48feeae31d8
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-08-04 13:09:30 +0900

    Tune database pool for stale connection recovery

--- src/main/java/com/zes/hmitomesbridgeserver/config/ZES_authenticationDataSource.java
+++ src/main/java/com/zes/hmitomesbridgeserver/config/ZES_authenticationDataSource.java
@@ -24,13 +24,18 @@ import javax.sql.DataSource;
 )
 public class ZES_authenticationDataSource
 {
+    private static final String MYSQL_DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+
     @Primary
     @Bean(name = "dataSource")
     public DataSource dataSource()
     {
         try
         {
+            // Load and register Connector/J before Hikari asks DriverManager for the JDBC URL driver.
+            Class.forName(MYSQL_DRIVER_CLASS_NAME);
             HikariConfig config = new HikariConfig("db_config/mysql.config");
+            config.setDriverClassName(MYSQL_DRIVER_CLASS_NAME);
             return new HikariDataSource(config);
         }
         catch (Exception e)
