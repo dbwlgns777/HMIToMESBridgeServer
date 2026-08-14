6dccd50c1e077365251660fc62c8d364f9adc06c
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-08-14 17:41:04 +0900

    Reset work mode after verified work completion

--- src/main/java/com/zes/hmitomesbridgeserver/opcua/ZES_opcUaWorkItemProvider.java
+++ src/main/java/com/zes/hmitomesbridgeserver/opcua/ZES_opcUaWorkItemProvider.java
@@ -7,4 +7,12 @@ public interface ZES_opcUaWorkItemProvider
     List<ZES_opcUaWorkItem> ZES_getWorkItemsByIctNumber(String ictNumber);
 
     ZES_opcUaWorkItemPage ZES_getWorkItemsByIctNumber(String ictNumber, int page, int size);
+
+    ZES_workHistoryState ZES_getLatestActiveWorkHistory(String workOrderCode);
+
+    ZES_workHistoryState ZES_getActiveWorkHistory(String workHistoryCode);
+
+    ZES_workHistoryState ZES_getOtherWorkingHistory(String companyCode, String workHistoryCode);
+
+    ZES_opcUaWorkItem ZES_getWorkItemByWorkOrderCode(String workOrderCode);
 }
